332367d91c4b84f1e45bb3e2c6d392b4d68509d0
Jeffrey Kegler <jkegl@cpan.org>
2008-09-29 04:29:25 +0000

    Import of JKEGL/Parse-Marpa-0.218000 from CPAN.
    gitpan-cpan-distribution: Parse-Marpa
    gitpan-cpan-version:      0.218000
    gitpan-cpan-path:         JKEGL/Parse-Marpa-0.218000.tar.gz
    gitpan-cpan-author:       JKEGL
    gitpan-cpan-maturity:     released

--- lib/Parse/Marpa/Raw_Source.pm
+++ lib/Parse/Marpa/Raw_Source.pm
@@ -19,10 +19,10 @@ my $new_default_null_value;
 my $new_default_lex_prefix;
 my %strings;
 
-# This file was automatically generated using Parse::Marpa 0.216
+# This file was automatically generated using Parse::Marpa 0.218
 $new_semantics = 'perl5';
 
-$new_version = '0.216000';
+$new_version = '0.218000';
 
 $new_start_symbol = "grammar";
 
